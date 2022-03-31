7fb9b78b8448a5bf0d13a68b186a3573ec09ec9f
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-03-31 20:35:31 +0800

    datamanager

---
+++ dotnet-mycompany/Domain/Repositories/DataManager.cs
@@ -0,0 +1,15 @@
+﻿using dotnet_mycompany.Domain.Repositories.Abstract;
+
+namespace dotnet_mycompany.Domain.Repositories
+{
+	public class DataManager
+	{
+		public ITextFieldsRepository TextFields { get; set; }
+		public IServiceItemsRepository ServiceItems { get; set; }
+		public DataManager(ITextFieldsRepository textFieldsRepository, IServiceItemsRepository serviceItemsRepository)
+		{
+			TextFields = textFieldsRepository;
+			ServiceItems = serviceItemsRepository;
+		}
+	}
+}
