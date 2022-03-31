fbc101b1d05d3fad8eec7f6502cdff343243ccea
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-03-31 18:25:37 +0800

    Доменные объекты

---
+++ dotnet-mycompany/Domain/Entities/TextField.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotnet_mycompany.Domain.Entities
+{
+	public class TextField : EntityBase
+	{
+		[Required]
+		public string CodeWord { get; set; }
+
+		[Display(Name = "Название страница (заголовок)")]
+		public override string Title { get; set; } = "Информационная страница";
+
+		[Display(Name = "Содержание страницы")]
+		public override string Text { get; set; } = "Содержание заполняется администратором";
+	}
+}
