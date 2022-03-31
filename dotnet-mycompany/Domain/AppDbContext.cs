74e8dd1fe6e910c9f3463fad1d029e5132330d0b
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-03-31 21:05:23 +0800

    guids

--- dotnet-mycompany/Domain/AppDbContext.cs
+++ dotnet-mycompany/Domain/AppDbContext.cs
@@ -18,14 +18,14 @@ namespace dotnet_mycompany.Domain
 
 			modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
 			{
-				Id = "12r23frwfsoi",
+				Id = "53a8d841-dd38-4044-8bcf-c3852ce19a1b",
 				Name = "admin",
 				NormalizedName = "ADMIN",
 			});
 
 			modelBuilder.Entity<IdentityUser>().HasData(new IdentityUser
 			{
-				Id = "adfsfdjckcz",
+				Id = "af278e2b-003d-4289-a11f-30ff88905804",
 				UserName = "admin",
 				NormalizedUserName = "ADMIN",
 				Email = "my@email.com",
@@ -37,27 +37,27 @@ namespace dotnet_mycompany.Domain
 
 			modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
 			{
-				RoleId = "12r23frwfsoi",
-				UserId = "adfsfdjckcz"
+				RoleId = "53a8d841-dd38-4044-8bcf-c3852ce19a1b",
+				UserId = "af278e2b-003d-4289-a11f-30ff88905804"
 			});
 
 			modelBuilder.Entity<TextField>().HasData(new TextField
 			{
-				Id = new Guid("g"),
+				Id = new Guid("1e6c1474-afc0-4b08-b08c-d7c280ead026"),
 				CodeWord = "PageIndex",
 				Title = "Главная"
 			});
 
 			modelBuilder.Entity<TextField>().HasData(new TextField
 			{
-				Id = new Guid("sgdf"),
+				Id = new Guid("373547d3-9332-4e25-a76b-267331844484"),
 				CodeWord = "PageServices",
 				Title = "Наши услуги"
 			});
 
 			modelBuilder.Entity<TextField>().HasData(new TextField
 			{
-				Id = new Guid("qwer"),
+				Id = new Guid("10058beb-6d89-47a0-98fb-d264dfb6fdff"),
 				CodeWord = "PageContacts",
 				Title = "Контакты"
 			});
