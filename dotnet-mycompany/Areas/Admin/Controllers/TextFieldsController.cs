4e70dacfb7e6e788da09e9a3f6ed399e872b3039
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-04-01 19:07:39 +0800

    Edit

---
+++ dotnet-mycompany/Areas/Admin/Controllers/TextFieldsController.cs
@@ -0,0 +1,33 @@
+﻿using dotnet_mycompany.Domain.Entities;
+using dotnet_mycompany.Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using dotnet_mycompany.Service;
+
+namespace dotnet_mycompany.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class TextFieldsController : Controller
+	{
+		private readonly DataManager dataManager;
+		public TextFieldsController(DataManager dataManager)
+		{
+			this.dataManager = dataManager;
+		}
+
+		public IActionResult Edit(string codeWord)
+		{
+			var entity = dataManager.TextFields.GetTextFieldByCodeWord(codeWord);
+			return View(entity);
+		}
+		[HttpPost]
+		public IActionResult Edit(TextField model)
+		{
+			if (ModelState.IsValid)
+			{
+				dataManager.TextFields.SaveTextField(model);
+				return RedirectToAction(nameof(HomeController.Index), nameof(HomeController).CutController());
+			}
+			return View(model);
+		}
+	}
+}
