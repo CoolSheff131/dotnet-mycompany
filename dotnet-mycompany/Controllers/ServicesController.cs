d4baf578250477ca2e14f8bf7103c1fd4ff90e52
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-04-01 21:28:48 +0800

    Services

---
+++ dotnet-mycompany/Controllers/ServicesController.cs
@@ -0,0 +1,24 @@
+﻿using dotnet_mycompany.Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_mycompany.Controllers
+{
+	public class ServicesController : Controller
+	{
+		private readonly DataManager dataManager;
+
+		public ServicesController(DataManager dataManager)
+		{
+			this.dataManager = dataManager;
+		}
+		public IActionResult Index(Guid id)
+		{
+			if(id != default)
+			{
+				return View("Show", dataManager.ServiceItems.GetServiceItemById(id));
+			}
+			ViewBag.TextField = dataManager.TextFields.GetTextFieldByCodeWord("PageServices");
+			return View(dataManager.ServiceItems.GetServiceItems());
+		}
+	}
+}
