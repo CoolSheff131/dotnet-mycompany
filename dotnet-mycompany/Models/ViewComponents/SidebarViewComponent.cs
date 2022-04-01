d090e245a4ad9f12ba6474da52e7200eaaad7670
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-04-01 21:44:41 +0800

    Sidebar

---
+++ dotnet-mycompany/Models/ViewComponents/SidebarViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using dotnet_mycompany.Domain.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_mycompany.Models.ViewComponents
+{
+	public class SidebarViewComponent : ViewComponent
+	{
+		private readonly DataManager dataManager;
+
+		public SidebarViewComponent(DataManager dataManager)
+		{
+			this.dataManager = dataManager;
+		}
+
+		public Task<IViewComponentResult> InvokeAsync()
+		{
+			return Task.FromResult((IViewComponentResult)View("default", dataManager.ServiceItems.GetServiceItems()));
+		}
+ 	}
+}
